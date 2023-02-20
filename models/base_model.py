0f6a3070acdba61e2ee976e2a4f02fdeacb4824c
Babashehu Shettima Musti <shettimababashehu57@gmail.com>
2023-02-20 03:21:22 +0100

    Changing from dict to class

--- models/base_model.py
+++ models/base_model.py
@@ -7,29 +7,33 @@ from datetime import datetime
 class BaseModel:
     """This defines all common attributes/methods for other classes"""
 
-    timestamp = datetime.now()
-    class_name = 'BaseModel'
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         """Initalizes a new instance"""
         self.id = uuid4().hex
-        self.created_at = self.timestamp
-        self.updated_at = self.timestamp
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+        for key, value in kwargs.items():
+            if key != '__class__':
+                if key in ['created_at', 'updated_at']:
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
 
     def __str__(self):
         """String representation of the instance"""
-        return ('[{}] ({}) {}'.format(self.class_name, self.id, self.__dict__))
+        return ('[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = self.timestamp
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """returns a dictionary containing all
         keys/values of __dict__ of the instance"""
         dict_repr = self.__dict__
-        dict_repr['__class__'] = self.class_name
+        dict_repr['__class__'] = self.__class__.__name__
         dict_repr['created_at'] = self.created_at.isoformat()
         dict_repr['updated_at'] = self.updated_at.isoformat()
         return dict_repr
